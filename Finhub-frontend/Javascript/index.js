bf32d3100a754cd84aa19e9c5836be35c60eeaa7
benny-png <mazikuben2@gmail.com>
2024-08-06 12:18:00 +0300

    new changes

---
+++ Finhub-frontend/Javascript/index.js
@@ -0,0 +1,35 @@
+const mynum=2 //contant
+var mynumber =67//can change
+
+
+console.log(typeof true)
+console.log(mynumber)
+console.log(mynum)
+
+mynumber ="Dr"
+console.log(mynumber)
+
+var arr =["name",1,true,3.56]
+console.log(arr)
+
+//object key value
+
+var myObject ={"User1":2,"Arr":[12, true, "John"],"Object":{"key":"Value"}}
+console.log(myObject)
+
+var myUser = {
+    "Name":"Joe",
+    "Age":34,
+    "Status":"Verified",
+    "Paid":true
+}
+
+console.log(myUser.Name)
+console.log(myUser.Age)
+console.log(myUser.Status)
+console.log(myUser.Paid)
+
+//operation
+console.log(4*6)
+
+
